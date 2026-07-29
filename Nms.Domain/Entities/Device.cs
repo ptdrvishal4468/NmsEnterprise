4b8e5edba8fe7a07d408774726d3e5ef776ed70b
= <patidarvishal4468@gmail.com>
2026-07-29 11:45:55 +0530

    feat(domain): implement core base entities, enums, tenant guards, and aggregate roots

---
+++ Nms.Domain/Entities/Device.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nms.Domain.Entities
+{
+    internal class Device
+    {
+    }
+}
---
+++ Nms.Domain/Enums/DeviceStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nms.Domain.Enums
+{
+    internal class DeviceStatus
+    {
+    }
+}
